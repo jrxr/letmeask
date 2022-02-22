a1c757673797f50d3de518e9fc6111e750404197
Agacy junior <juniormartinssom@gmail.com>
2022-02-22 15:34:28 -0300

    Finish function like

--- src/Pages/AdminRoom.tsx
+++ src/Pages/AdminRoom.tsx
@@ -18,36 +18,9 @@ type RoomParams = {
 export function AdminRoom() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
-  const [newQuestion, setNewQuestion] = useState('');
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId);
-
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault();
-
-    if (newQuestion.trim() === '') {
-      return;
-    }
-
-    if (!user) {
-      throw new Error('You must be logged in');
-    }
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar
-      },
-      isHighlighted: false,
-      isAnswered: false
-    };
-
-    await database.ref(`rooms/${roomId}/questions`).push(question);
-
-    setNewQuestion('');
-  }
 
   return (
     <div id="page-room">
